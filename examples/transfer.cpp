b8951ff3cec836839bf475644eb7b63a4bb2a8f6
root <root@DESKTOP-V7A7NAR>
2024-02-24 00:38:27 +0800

    Add examples.

---
+++ examples/transfer.cpp
@@ -0,0 +1,31 @@
+#include "stdexec/execution.hpp"
+#include "asio2exec.hpp"
+#include "asio/steady_timer.hpp"
+#include "asio/post.hpp"
+#include <iostream>
+
+namespace ex = stdexec;
+
+int main(){
+    std::cout << "Main thread: " << std::this_thread::get_id() << std::endl;
+
+    asio2exec::asio_context ctx;
+    ctx.start();
+
+    asio::post(ctx.get_executor(), [](){
+        std::cout << "IO context thread: " << std::this_thread::get_id() << std::endl;
+    });
+
+    asio::steady_timer timer{ctx.get_executor(), std::chrono::seconds(1)};
+
+    auto work = timer.async_wait(asio2exec::use_sender) |
+                ex::then([](asio::error_code ec){
+                    std::cout << "Running in main thread:" << std::this_thread::get_id() << std::endl;
+                }) |
+                ex::transfer(ctx.get_scheduler()) |
+                ex::then([]{
+                    std::cout << "Running in io context thread:" << std::this_thread::get_id() << std::endl;
+                });
+
+    ex::sync_wait(std::move(work));
+}
